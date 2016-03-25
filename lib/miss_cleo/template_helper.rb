4e8fadcb1f2ee92a43865d465a56869e7f90b1cc
Dean Hu <zephyr-eng+dean@gust.com>
2016-03-25 18:17:53 +0000

    Fix template #reset_coverage bug

--- lib/miss_cleo/template_helper.rb
+++ lib/miss_cleo/template_helper.rb
@@ -3,15 +3,15 @@ module MissCleo
     module_function
 
     def template_coverage
-      @template_coverage ||= []
+      @_template_coverage ||= []
     end
 
     def add_to_template_coverage(template)
-      template_coverage << trim_path(template)
+      @_template_coverage << trim_path(template)
     end
 
     def reset_coverage
-      template_coverage = []
+      @_template_coverage = []
     end
 
     def trim_path(path)
